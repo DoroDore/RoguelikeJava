8835f49bb2e924941fb85ada8b4e50956b58797e
DoroDore <tlyz82@student.cis.edu.hk>
2023-09-14 12:18:59 +0800

    Version -1.1, added attacking structure, monster priming, weapons, and battle system.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static int hp = 10;
+    public static int baseMana = 20;
+    public static int mana;
+    public static int attack;
+    public static String weapon = "Sharpened Stick";
+    public static int worldNo = 1;
+    public static int wave = 1;
+    public static String enemy;
+    public static int enemyAttack;
+    public static int enemyHP;
     public static void main(String[] args) {
         introduction();
-        characterInitialization();
         System.out.println("\n\n\n\n\n------------------------------");
         System.out.println("You wake up in a strange forest, with nothing but a sharpened wooden stick.");
         stall();
@@ -15,23 +24,47 @@ public class Main {
         System.out.println("Cautiously, you pick up your sharpened stick and creep towards the entity.");
         stall();
         System.out.println("You encountered a blue slime!");
-
+        primeEnemy();
+        actions();
     }
-    public static void actions(){
-        System.out.println("HP:"+HP); //Fix this by moving the variable into main thanks
+    public static void primeEnemy() {
+        preBattleCheck();
+        if (worldNo == 1 && wave == 1) {
+            enemy = "Blue Slime";
+            enemyAttack = 1;
+            enemyHP = 10;
+        }
+    }
+    public static void preBattleCheck() {
+        if (weapon.equals("Sharpened Stick")) {
+            attack = 5;
+            mana = baseMana;
+        }
+    }
+    public static void actions() {
+        System.out.println("ATK: " + attack + "HP: " + hp + "\tMana: " + mana + "\tWeapon: " + weapon);
         System.out.println("What would you like to do?");
-        System.out.println("")
+        System.out.println("[A] To Attack or [S] to Bring up Spells list");
+        Scanner userChoice = new Scanner(System.in);
+        String userInput;
+        boolean validInput = false;
+        String userInputUpper;
+        while (!validInput) {
+            try {
+                userInput = userChoice.next();
+                userInputUpper = userInput.toUpperCase();
+                if (userInputUpper.equals("A") || userInputUpper.equals("S")) {
+                    validInput = true;
+                } else {
+                    throw new InputMismatchException();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please input a valid answer.");
+            }
+        }
+        System.out.println("Success!");
     }
-    public static void stall() {
-        Scanner stall = new Scanner(System.in);
-        stall.nextLine();
-    }
-    public static void characterInitialization() {
-        int HP = 10;
-        int Mana = 20;
-        String weapon = "Sharpened Stick";
-    }
-    public static void introduction() {
+    public static void introduction () {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome adventurer to the text based version of Roguelike Adventures!");
         System.out.println("Before we begin, what is your name?");
@@ -47,7 +80,7 @@ public class Main {
         }
         System.out.println("Wait. One more thing. Do you know the rules of this game?");
         System.out.println("[Y] for yes, [N] for no.");
-        String yesNo = null;
+        String yesNo;
         boolean validAnswer = false;
         String yesNoUpper = null;
         while (!validAnswer) {
@@ -56,12 +89,10 @@ public class Main {
                 yesNoUpper = yesNo.toUpperCase();
                 if (yesNoUpper.equals("Y") || yesNoUpper.equals("N")) {
                     validAnswer = true;
-                }
-                else {
+                } else {
                     throw new InputMismatchException();
                 }
-            }
-            catch (InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Please input a valid answer.");
             }
         }
@@ -94,5 +125,9 @@ public class Main {
             System.out.println("Very well. We shall begin.");
             stall();
         }
+    }
+    public static void stall () {
+        Scanner stall = new Scanner(System.in);
+        stall.nextLine();
     }
 }
